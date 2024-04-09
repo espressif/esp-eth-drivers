66b4d8d39a5841e5ae328be188a3ac798ba6f3c6
Bogdan Kolendovskyy <bogdan.kolendovskyy@espressif.com>
2024-04-09 12:03:52 +0200

    Add multidrop support to LAN867x tcp transfer example

--- lan867x/examples/lan867x_tcp_sockets/client/main/client.c
+++ lan867x/examples/lan867x_tcp_sockets/client/main/client.c
@@ -4,10 +4,12 @@
 #include "freertos/task.h"
 #include "esp_netif.h"
 #include "esp_eth.h"
+#include "esp_eth_phy_lan867x.h"
 #include "esp_event.h"
 #include "esp_log.h"
 #include "ethernet_init.h"
 #include "lwip/sockets.h"
+#include "sdkconfig.h"
 
 #define SOCKET_ADDRESS      "192.168.1.1"
 #define SOCKET_PORT         5000
@@ -49,8 +51,21 @@ void app_main(void)
     ESP_ERROR_CHECK(esp_netif_attach(eth_netif, esp_eth_new_netif_glue(eth_handles[0])));
     // Register user defined event handers
     ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_ETH_GOT_IP, &got_ip_event_handler, NULL));
+    // Configure PLCA
+#if CONFIG_EXAMPLE_LAN867X_USE_PLCA
+    // Configure PLCA with node number from config
+    uint8_t plca_id = CONFIG_EXAMPLE_LAN867X_PLCA_ID;
+    esp_eth_ioctl(eth_handles[0], LAN867X_ETH_CMD_S_PLCA_ID, &plca_id);
+    bool plca_en = true;
+    esp_eth_ioctl(eth_handles[0], LAN867X_ETH_CMD_S_EN_PLCA, &plca_en);
+#endif // otherwise rely on CSMA/CD
     // Start Ethernet driver
     esp_eth_start(eth_handles[0]);
+    // Get mac address and save it as a string
+    char mac_str[18];
+    uint8_t mac_data[6];
+    esp_eth_ioctl(eth_handles[0], ETH_CMD_G_MAC_ADDR, &mac_data);
+    sprintf(mac_str, "%02x:%02x:%02x:%02x:%02x:%02x", mac_data[0], mac_data[1], mac_data[2], mac_data[3], mac_data[4], mac_data[5]);
     // Initialize Berkley socket
     char txbuffer[SOCKET_MAX_LENGTH] = {0};
     int client_fd = socket(AF_INET, SOCK_STREAM, 0);
@@ -61,10 +76,10 @@ void app_main(void)
     // Wait until IP address is assigned to this device
     xSemaphoreTake(xGotIpSemaphore, portMAX_DELAY);
     int transmission_cnt = 0;
+    connect(client_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
     while (1) {
-        connect(client_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
-        snprintf(txbuffer, SOCKET_MAX_LENGTH, "Transmission #%d. Hello from ESP32 via LAN867x", ++transmission_cnt);
-        int bytesSent = send(client_fd, txbuffer, strlen(txbuffer), 0);
+        snprintf(txbuffer, SOCKET_MAX_LENGTH, "Transmission #%d. Hello from ESP32 (%s) via LAN867x", ++transmission_cnt, mac_str);
+        int bytesSent = send(client_fd, txbuffer, SOCKET_MAX_LENGTH, 0);
         ESP_LOGI(TAG, "Sent tranmission #%d which was %d bytes long.", transmission_cnt, bytesSent);
         vTaskDelay(pdMS_TO_TICKS(500));
     }
