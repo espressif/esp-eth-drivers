2875b4406a91761ae816dd450056ef5a891cc143
Ondrej Kosta <ondrej.kosta@espressif.com>
2025-06-10 15:16:49 +0200

    feat(lan867x,lan86xx): separated core PHY functionality to separate common folder
    BREAKING CHANGE: new esp_eth_ioctl command naming

--- lan867x/include/esp_eth_phy_lan867x.h
+++ lan867x/include/esp_eth_phy_lan867x.h
@@ -1,34 +1,16 @@
 /*
- * SPDX-FileCopyrightText: 2023 Espressif Systems (Shanghai) CO LTD
+ * SPDX-FileCopyrightText: 2023-2025 Espressif Systems (Shanghai) CO LTD
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
 #pragma once
 
-#include "esp_eth_phy.h"
+#include "esp_eth_phy_lan86xx.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-typedef enum {
-    LAN867X_ETH_CMD_S_EN_PLCA = ETH_CMD_CUSTOM_PHY_CMDS, /*!< Enable or disable PLCA */
-    LAN867X_ETH_CMD_G_EN_PLCA,                           /*!< Get status if PLCA is disabled or enabled*/
-    LAN867X_ETH_CMD_S_PLCA_NCNT,                         /*!< Set PLCA node count */
-    LAN867X_ETH_CMD_G_PLCA_NCNT,                         /*!< Get PLCA node count */
-    LAN867X_ETH_CMD_S_PLCA_ID,                           /*!< Set PLCA ID */
-    LAN867X_ETH_CMD_G_PLCA_ID,                           /*!< Get PLCA ID */
-    LAN867X_ETH_CMD_S_PLCA_TOT,                          /*!< Set PLCA Transmit Opportunity Timer in incriments of 100ns */
-    LAN867X_ETH_CMD_G_PLCA_TOT,                          /*!< Get PLCA Transmit Opportunity Timer in incriments of 100ns */
-    LAN867X_ETH_CMD_ADD_TX_OPPORTUNITY,                  /*!< Add additional transmit opportunity for chosen node */
-    LAN867X_ETH_CMD_RM_TX_OPPORTUNITY,                   /*!< Remove additional transmit opportunity for chosen node */
-    LAN867X_ETH_CMD_S_MAX_BURST_COUNT,                   /*!< Set max count of additional packets, set to 0 to disable */
-    LAN867X_ETH_CMD_G_MAX_BURST_COUNT,                   /*!< Get max count of additional packets, set to 0 to disable */
-    LAN867X_ETH_CMD_S_BURST_TIMER,                       /*!< Set time after transmission during which node is allowed to transmit more packets in incriments of 100ns */
-    LAN867X_ETH_CMD_G_BURST_TIMER,                       /*!< Get time after transmission during which node is allowed to transmit more packets in incriments of 100ns */
-    LAN768X_ETH_CMD_PLCA_RST                             /*!< Reset PLCA*/
-} phy_lan867x_custom_io_cmd_t;
 
 /**
 * @brief Create a PHY instance of LAN867x
